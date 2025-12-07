bb4442e7513e78104f24a39910ebe7174f2670b6
hbenarous <hbenarous@weborama.com>
2025-12-07 19:50:20 +0100

    Cleanup: remove duplicate files and unused code, update README

--- server/src/middlewares/validate.middleware.js
+++
@@ -1,8 +0,0 @@
-export function validate(schema) {
-	return (req, res, next) => {
-		const { error, value } = schema.validate(req.body);
-		if (error) return res.status(400).json({ error: error.message });
-		req.validatedBody = value;
-		return next();
-	};
-}
